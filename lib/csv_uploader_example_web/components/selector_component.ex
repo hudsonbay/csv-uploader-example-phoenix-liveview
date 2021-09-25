560d95e35854dd956123126aec3e6c66fa55d159
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-09-25 01:05:05 -0400

    feat: :sparkles: allowed uploads on selector component

--- lib/csv_uploader_example_web/components/selector_component.ex
+++ lib/csv_uploader_example_web/components/selector_component.ex
@@ -1,12 +1,24 @@
 defmodule SelectorComponent do
   use CsvUploaderExampleWeb, :live_component
 
-  def render(assigns) do
-  ~H"""
-  <article class="column">
-    <h2>Selector</h2>
+  @impl true
+  def mount(socket) do
+    {:ok,
+     socket
+     |> allow_upload(:csv, accept: ~w(.csv), max_entries: 1)}
+  end
 
-  </article>
-  """
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <article class="column">
+      <h2>Selector</h2>
+      <form id="upload-form" phx-submit="save" phx-change="validate">
+        <%= live_file_input @uploads.csv %>
+        <button type="submit">Upload</button>
+      </form>
+
+    </article>
+    """
   end
 end
--- lib/csv_uploader_example_web/live/homepage_live/index.ex
+++ lib/csv_uploader_example_web/live/homepage_live/index.ex
@@ -1,8 +1,3 @@
 defmodule CsvUploaderExampleWeb.HomepageLive.Index do
   use CsvUploaderExampleWeb, :live_view
-
-  @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, %{})}
-  end
 end
