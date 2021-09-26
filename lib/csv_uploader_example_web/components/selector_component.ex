e2b88c0ad83f79e128eb86493d6d007bb9b32cd8
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-09-26 01:03:35 -0400

    refactor: :fire: Moved CSV handling imlementation from component

--- lib/csv_uploader_example_web/components/selector_component.ex
+++ lib/csv_uploader_example_web/components/selector_component.ex
@@ -1,24 +1,11 @@
 defmodule SelectorComponent do
   use CsvUploaderExampleWeb, :live_component
 
-  @impl true
-  def mount(socket) do
-    {:ok,
-     socket
-     |> allow_upload(:csv, accept: ~w(.csv), max_entries: 1)}
-  end
 
   @impl true
   def render(assigns) do
     ~H"""
-    <article class="column">
-      <h2>Selector</h2>
-      <form id="upload-form" phx-submit="save" phx-change="validate">
-        <%= live_file_input @uploads.csv %>
-        <button type="submit">Upload</button>
-      </form>
-
-    </article>
+    <div></div>
     """
   end
 end
