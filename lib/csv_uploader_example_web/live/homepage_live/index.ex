8c45f49d4df3e797e53a7bb083468cc825951d8e
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-10-03 03:23:47 -0400

    feat: :sparkles: formatted content from CSV and inserted it into the DB

--- lib/csv_uploader_example_web/live/homepage_live/index.ex
+++ lib/csv_uploader_example_web/live/homepage_live/index.ex
@@ -1,10 +1,16 @@
 defmodule CsvUploaderExampleWeb.HomepageLive.Index do
   use CsvUploaderExampleWeb, :live_view
 
+  alias CsvUploaderExample.Branches
+  alias CsvUploaderExample.Branches.Branch
+  alias CsvUploaderExample.Repo
+  alias CsvUploaderExample.Utils
+
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     {:ok,
      socket
+     |> assign(branches: Branches.list_branches())
      |> allow_upload(:csv, accept: ~w(.csv), max_entries: 1)}
   end
 
@@ -13,7 +19,8 @@ defmodule CsvUploaderExampleWeb.HomepageLive.Index do
     consume_uploaded_entries(socket, :csv, fn meta, entry ->
       dest = Path.join("priv/static/uploads", "#{entry.uuid}.csv")
       File.cp!(meta.path, dest)
-      read_csv(dest)
+      contents = read_csv(dest)
+      Repo.insert_all(Branch, contents)
     end)
 
     {:noreply, socket}
@@ -29,6 +36,18 @@ defmodule CsvUploaderExampleWeb.HomepageLive.Index do
     |> File.stream!()
     |> CSV.decode(separator: ?|, headers: true)
     |> Enum.map(fn {:ok, branch} -> branch end)
-    |> IO.inspect()
+    |> format_content()
+  end
+
+  defp format_content(branch_list_map) do
+    branch_list_map
+    |> Enum.map(fn map ->
+      formatted_map =
+        map
+        |> Map.new(fn {k, v} -> {String.downcase(k), v} end)
+        |> Utils.cast_map()
+
+      %{formatted_map | part_price: Decimal.new(formatted_map[:part_price])}
+    end)
   end
 end
