dde7001bb1248aceae5d29e3ff4fc10d21f2848d
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-10-04 01:04:36 -0400

    feat: :sparkles: added an npx watcher that runs the tailwindcss utility

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :csv_uploader_example, CsvUploaderExampleWeb.Endpoint,
   secret_key_base: "QkTus6b2sseSEmFxKT351oOmYb+G178ZbTDn4gdenYvz96mmBckvlcNVNcxauOLG",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
